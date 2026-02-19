3994fa08ef4324649d9dae90d0ca5786b20262c2
kondrse <kondor85@mail.ru>
2026-02-19 16:21:06 +0300

    fix refer

--- app/api/results/route.ts
+++ app/api/results/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest) {
           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
           'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
           'Accept-Encoding': 'gzip, deflate, br',
-          'Referer': 'https://c-f-r.ru/'
+          'Referer': 'http://c-f-r.ru/'
         },
         maxRedirects: 10
       })
