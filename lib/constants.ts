311e4bd436fbf9da46997d90d81c8e340922e089
kondrse <kondor85@mail.ru>
2026-02-19 16:17:27 +0300

    http

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const DEFAULT_URL_CODE = '2602vrn'
 export const DEFAULT_CITY = 'СПБ'
 export const UPDATE_INTERVAL = 120000 
 export const DEBOUNCE_DELAY = 500
-export const EXTERNAL_API_BASE_URL = 'https://c-f-r.ru/live/'
+export const EXTERNAL_API_BASE_URL = 'http://c-f-r.ru/live/'
 
 export const DISCIPLINES = {
   LEAD: 'трудность' as const,
