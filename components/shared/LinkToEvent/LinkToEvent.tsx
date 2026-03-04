bd60175be1dc44bb7708067a1781fb68c034f640
kondrse <kondor85@mail.ru>
2026-03-04 23:26:55 +0300

    fix

--- components/shared/LinkToEvent/LinkToEvent.tsx
+++ components/shared/LinkToEvent/LinkToEvent.tsx
@@ -1,10 +1,27 @@
-import { Status } from '@/types'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faCircle } from '@fortawesome/free-solid-svg-icons'
-import { STATUSES } from '@/lib/constants';
+'use client'
 
-export default function LinkToEvent({ status, onlyOnline }: { status: Status, onlyOnline?: boolean }): JSX.Element | null {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkSquare } from '@fortawesome/free-solid-svg-icons'
+import { EXTERNAL_API_BASE_URL } from '@/lib/constants';
+import { rootStore } from '@/lib/store/root.store';
+
+interface LinkToEventProps {
+  code: string | null
+}
+
+export default function LinkToEvent({ code }: LinkToEventProps): JSX.Element | null {
+  const store = rootStore.eventsStore
+  if (!code || !store.events) return null
+  const hasLink = store.events?.find((event) => event.link === code)
+  if (!hasLink) return null
   return (
-    <div className="w-4 mr-1"><FontAwesomeIcon icon={icon} className={`text-green-500 ${isOnline ? 'animate-ping' : ''}`} /></div>
+    <a
+      href={`${EXTERNAL_API_BASE_URL}${code}/index.html`}
+      className="block text-2xl mt-8 absolute top-[2px] right-10 z-20"
+      target="_blank"
+      title="открыть на сайте федерации скалолазанья"
+    >
+      <FontAwesomeIcon icon={faExternalLinkSquare} className={`text-teal-500 hover:text-blue-700 cursor-pointer`}  />
+    </a>
   )
 }
